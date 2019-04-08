f461aea134625691be7c52d15010fa3173205837
Ganna Simonova <anya_simonova@yahoo.com>
2019-04-08 22:53:31 +0300

    Home page layout update

--- action.js
+++ action.js
@@ -3,75 +3,64 @@
 const htmlBlock = document.querySelector('html');
 const bodyBlock = document.querySelector('body');
 const mainContainer = document.querySelector('.main_container');
-const gridBlocks = document.querySelectorAll('main > div:not(.portfolio)');
+const footer = document.querySelector('footer');
+const gridBlocks = document.querySelectorAll('main > *:not(.portfolio):not(.who_we_are)');
+const modalBlock = document.querySelector('.modal');
 const cursorDefault = document.querySelector('.cursor_outer');
 const cursorHover = document.querySelector('.cursor_inner');
 
 document.body.onload = function() {
-    popupBlockDescription();
+    if (navigator.userAgent.search("Safari") >= 0 && navigator.userAgent.search("Chrome") < 0) {
+        document.querySelector('main').style = "padding-bottom: 130px";
+        footer.style = "bottom:-5px; height: 85px";
+    }
+
+    gridBlocks.forEach(function(item) {
+        if (window.location.hash === item.dataset.hash) {
+            popupBlockDescription(item);
+        }
+    });
+    gridBlocks.forEach(function(item) {
+        item.addEventListener('click', function() {
+            window.location.hash = item.dataset.hash;
+            popupBlockDescription(item);
+        });
+    });
+
     window.onmousemove = (e) => mouseCoordinates(e);
 };
 
-function popupBlockDescription() {
-    gridBlocks.forEach(function(item) {
-        item.addEventListener('click', function() {
-            let blockTitle = item.querySelector('.block_title').innerHTML;
-            let blockSubtitle = item.querySelector('.block_subtitle');
-            let blockDescr = item.querySelector('.block_descr');
+function popupBlockDescription(item) {
+    let blockTitle = item.querySelector('.block_title').innerHTML;
+    let blockSubtitle = item.querySelector('.block_subtitle');
+    let blockDescr = item.querySelector('.block_descr');
 
-            let modalWrapper = document.createElement('div');
-            modalWrapper.setAttribute('class', 'modal');
-            let modal = document.createElement('div');
-            modal.setAttribute('class', 'pop-up');
+    modalBlock.querySelector(".block_title").innerHTML = blockTitle;
+    modalBlock.querySelector(".block_subtitle").innerHTML = blockSubtitle ? blockSubtitle.innerHTML : "";
+    modalBlock.querySelector(".block_descr").innerHTML = blockDescr ? blockDescr.innerHTML : "";
 
-            let closeIcon = document.createElement('img');
-            closeIcon.setAttribute('src', './images/close_icon.svg');
-            closeIcon.setAttribute('class', 'popup-close-btn cursor_links_hover');
-            closeIcon.setAttribute('onclick', 'hidePopup()');
-            modal.appendChild(closeIcon);
-
-            let modalTitle = document.createElement('div');
-            modalTitle.setAttribute('class', 'block_title');
-            modalTitle.innerHTML = blockTitle;
-            modal.appendChild(modalTitle);
-
-            if (blockSubtitle) {
-                let modalSubtitle = document.createElement('div');
-                modalSubtitle.setAttribute('class', 'block_subtitle');
-                modalSubtitle.innerHTML = blockSubtitle.innerHTML;
-                modal.appendChild(modalSubtitle);
-            }
-
-            let hr = document.createElement('hr');
-            hr.setAttribute('class', 'hr');
-            modal.appendChild(hr);
-
-            if (blockDescr) {
-                let modalDescr = document.createElement('div');
-                modalDescr.setAttribute('class', 'block_descr');
-                modalDescr.innerHTML = blockDescr.innerHTML;
-                modal.appendChild(modalDescr);
-            }
-
-            modalWrapper.appendChild(modal);
-            bodyBlock.appendChild(modalWrapper);
-            htmlBlock.style.overflow = 'hidden';
-            bodyBlock.style.overflow = 'scroll';
-            mainContainer.classList.add('blurred');
-        });
-    });
+    modalBlock.style.display = "block";
+    htmlBlock.style.overflowY = 'hidden';
+    htmlBlock.style.position = 'fixed';
+    bodyBlock.style.overflowY = 'scroll';
+    mainContainer.classList.add('blurred');
+    footer.classList.add('blurred');
 }
 
 function hidePopup() {
-    let modalWrapper = document.querySelector('.modal');
-    let modal = document.querySelector('.pop-up');
-    modal.classList.add('modal_up');
-    modalWrapper.classList.add("hidden");
-    setTimeout(function(){ bodyBlock.removeChild(modalWrapper);
+    history.pushState("", document.title, window.location.pathname);
+    modalBlock.classList.add('modal_up');
+    setTimeout(function() {
+        modalBlock.style.display = "none";
         mainContainer.classList.remove('blurred');
-        htmlBlock.style.overflow = 'auto';
-        bodyBlock.style.overflow = 'auto';
+        footer.classList.remove('blurred');
+        htmlBlock.style.overflowY = 'auto';
+        htmlBlock.style.position = 'inherit';
+        bodyBlock.style.overflowY = 'visible';
     }, 100);
+    setTimeout(function() {
+        modalBlock.classList.remove('modal_up');
+    }, 200);
 }
 
 
@@ -81,15 +70,13 @@ function mouseCoordinates(e) {
     cursorDefault.style.left = (e.pageX - 25) + 'px';
     cursorDefault.style.top = (e.pageY - 25) + 'px';
 
-    if (targetElem.classList.contains("cursor_links_hover")) {
+    if (targetElem.classList.contains("cursor_links_hover") ||
+        (targetElem.classList.contains("swiper-pagination-bullet") && !targetElem.classList.contains("swiper-pagination-bullet-active"))) {
         cursorHover.classList.add("links_hover");
         cursorDefault.classList.add("is_transparent");
     } else if (targetElem.classList.contains("cursor_content_hover")) {
         cursorHover.classList.add("content_hover");
         cursorDefault.classList.add("content_hover_inner");
-    } else if (targetElem.classList.contains("cursor_nav_hover")) {
-        cursorHover.classList.add("nav_hover");
-        cursorDefault.classList.add("is_transparent");
     } else {
         if (cursorHover.classList.contains("links_hover")) {
             cursorHover.classList.remove("links_hover");
@@ -102,3 +89,17 @@ function mouseCoordinates(e) {
         cursorDefault.classList.remove("is_transparent");
     }
 }
+
+var swiper = new Swiper('.blog-slider', {
+    spaceBetween: 30,
+    effect: 'fade',
+    loop: true,
+    mousewheel: {
+        invert: false,
+    },
+    // autoHeight: true,
+    pagination: {
+        el: '.blog-slider__pagination',
+        clickable: true,
+    }
+});
